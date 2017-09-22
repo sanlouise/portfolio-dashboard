5c720a1d11d43e363d6514223f2a659b9a4ddacd
sanlouise <sandra.adamshallie@gmail.com>
2017-09-22 11:57:20 -0400

    Add IntroText component

---
+++ src/pages/Dashboard/IntroText.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const IntroText = () => (
+  <div className="card card-user">
+    <div className="content">
+      <h2>Hello friend!</h2>
+      <h4>Welcome to my page.</h4>
+    </div>
+  </div>
+);
+
+export default IntroText;
