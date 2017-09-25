b84d35f760347b8a9bab92f28bf5042e85064374
sanlouise <sandra.adamshallie@gmail.com>
2017-09-25 11:50:08 -0400

    Add initial project data

---
+++ src/assets/data/projects.js
@@ -0,0 +1,27 @@
+const projects = [
+  {
+    id: 1,
+    name: "Dressy - Ecommerce App",
+    technologies: ["React", "Redux"],
+    description: "This is a test",
+    points: ["Developed from scratch with scaffolding.", "Designed UI with custom CSS, Sass, Twitter Bootstrap and JavaScript/JQuery.",
+    "Used Yahoo API for tracking stock markets.", "Applied Devise gem for user authentication.",
+    "Implemented auto complete search function using AJAX.", "Deployed application using Heroku with PostgreSQL."],
+    src: "https://github.com/sanlouise/ecommerce-react/raw/master/src/assets/images/readme.png",
+    github: "https://github.com/sanlouise/ecommerce-react/",
+    url: "https://ecommerce-react-redux.herokuapp.com/",
+  },
+  {
+    id: 2,
+    name: "Stock Tracker",
+    technologies: ["Ruby on Rails"],
+    description: "Users can easily search and track stocks. They can also follow what their friends are tracking.",
+    src: "http://www.sandrahallie.com/assets/Stocktracker_s-515adf869ce1d5580da6ae381a9bdc89adadbee0715fb366aba1f10a9e3e5bd9.jpg",
+    github: "https://github.com/sanlouise/stocktracker",
+    url: "http://socialstocktracker.herokuapp.com/",
+  },
+
+
+];
+
+export default projects;
