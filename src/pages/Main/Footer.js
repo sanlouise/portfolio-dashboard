447e7891db644a2c422d8fc8be2f2f17f176d573
sanlouise <sandra.adamshallie@gmail.com>
2017-09-26 16:44:11 -0400

    Add codewars badge to footer

--- src/pages/Main/Footer.js
+++ src/pages/Main/Footer.js
@@ -5,6 +5,9 @@ const Footer = () => (
     <div className="container-fluid">
       <p className="copyright text-center">
         © 2017 Sandra Hallie
+        <a href="https://www.codewars.com/users/sanlouise">
+          <img src="https://www.codewars.com/users/sanlouise/badges/small" />
+        </a>
       </p>
     </div>
   </footer>
